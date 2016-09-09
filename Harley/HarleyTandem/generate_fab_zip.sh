847ff94e52fd1b5f61b64e79cfe64366e77de273
Morgan Quigley <morgan@osrfoundation.org>
2016-09-09 14:09:15 -0700

    minor cosmetic tweaks to layout and BOM

--- Harley/HarleyTandem/generate_fab_zip.sh
+++ Harley/HarleyTandem/generate_fab_zip.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -o errexit
 set -o verbose
-FAB=harley_tandem_rev_a
+FAB=harley_tandem_rev_b
 rm -rf $FAB
 mkdir $FAB
 cp gerbers/* $FAB
 cp README.txt $FAB
-cp harley_tandem_stackup.pdf ${FAB}/${FAB}__pcb_stackup.pdf
+cp harley_tandem_stackup_rev_b.pdf ${FAB}/${FAB}__pcb_stackup.pdf
 cp HarleyTandem.csv ${FAB}/${FAB}__bill_of_materials.csv
 rm -f $FAB.zip
 zip -r $FAB.zip $FAB
