5b9bb371414b5a602e9c3392055ad04d8594ea9b
Morgan Quigley <morgan@osrfoundation.org>
2018-05-14 14:47:06 -0700

    routing and script file location tweak

--- hardware/ovc2/stereo/fpga_power.sch
+++ hardware/ovc2/stereo/fpga_power.sch
@@ -1,5 +1,4 @@
 EESchema Schematic File Version 4
-LIBS:ovc2_stereo-cache
 EELAYER 26 0
 EELAYER END
 $Descr A2 16535 23386 portrait
--- software/ovc1/scripts/flash_fpga_config.sh
+++ software/ovc1/scripts/flash_fpga_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -o verbose
-~/opencam/software/mcu_cli/bin/mcu_cli flash_fpga_image /home/nvidia/opencam/firmware/ovc/fpga/stable/ovc.periph.rbf
+~/ovc/software/ovc1/mcu_cli/bin/mcu_cli flash_fpga_image /home/nvidia/opencam/firmware/ovc/fpga/stable/ovc.periph.rbf
