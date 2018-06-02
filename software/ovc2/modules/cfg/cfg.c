b38ae14eb17c36430047f3725415b23f0050ec8f
Morgan Quigley <morgan@osrfoundation.org>
2018-06-02 06:31:34 +0000

    WIP configuration module

---
+++ software/ovc2/modules/cfg/cfg.c
@@ -0,0 +1,52 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/spi/spi.h>
+
+static struct spi_device *device;
+
+static int __init cfg_init(void)
+{
+  int rc;
+  unsigned char c = 0x12;
+  struct spi_master *master;
+  struct spi_board_info board_info = {
+    //.modalias = "cfg",
+    .max_speed_hz = 10000000,
+    .bus_num = 3,
+    .chip_select = 0,
+    .mode = 0
+  };
+  master = spi_busnum_to_master(board_info.bus_num);
+  if (!master) {
+    printk("spi_busnum_to_master() failed\n");
+    return -ENODEV;
+  }
+  device = spi_new_device(master, &board_info);
+  if (!device) {
+    printk("spi_new_device() failed\n");
+    return -ENODEV;
+  }
+  device->bits_per_word = 8;
+  rc = spi_setup(device);
+  if (rc) {
+    printk("spi_setup() failed\n");
+    spi_unregister_device(device);
+    return -ENODEV;
+  }
+  spi_write(device, &c, 1);
+  return 0;
+}
+
+static void __exit cfg_exit(void)
+{
+  if (device) {
+    spi_unregister_device(device);
+  }
+}
+
+module_init(cfg_init);
+module_exit(cfg_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Morgan Quigley <morgan@openrobotics.org>");
+MODULE_DESCRIPTION("SPI configuration driver for Altera Cyclone 10 GX");
