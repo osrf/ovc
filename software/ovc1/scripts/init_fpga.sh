9346374befed5f30452454d4e75d88446259c6d9
FLA <fla-falcon-deploy@users.noreply.github.com>
2019-01-11 12:12:37 -0500

    Updated init_fpga to only allow 1 run

--- software/ovc1/scripts/init_fpga.sh
+++ software/ovc1/scripts/init_fpga.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-set -o verbose
-cd ~/ovc/software/ovc1/ovc_module
-sudo ./ovc_load
-sleep 3
-cp /home/nvidia/ovc/firmware/ovc1/fpga/stable/ovc.core.rbf /dev/ovc_cvp
-sleep 3
-sudo rmmod ovc
-sudo ./ovc_load
+file=/tmp/ovc_fpga_init
+
+if [ ! -f $file ]; then
+  echo "OVC_FPGA_INIT=true" >> /tmp/ovc_fpga_init
+  echo "Initializing FPGA..."
+  echo "save state at: $file"
+  set -o verbose
+  cd ~/ovc/software/ovc1/ovc_module
+  sudo ./ovc_load
+  sleep 3
+  cp ~/ovc/firmware/ovc1/fpga/stable/ovc.core.rbf /dev/ovc_cvp
+  sleep 3
+  sudo rmmod ovc
+  sudo ./ovc_load
+fi
