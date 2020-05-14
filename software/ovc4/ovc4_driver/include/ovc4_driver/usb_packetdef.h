55a0fe34c981e38d1ef8e080585aeb50d1e98dd7
Luca Della Vedova <luca@openrobotics.org>
2020-05-14 15:19:35 +0800

    Skeleton of PC USB interface working
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

---
+++ software/ovc4/ovc4_driver/include/ovc4_driver/usb_packetdef.h
@@ -0,0 +1,1 @@
+/home/luca/hw/ovc/firmware/ovc4/lpc_fw/include/usb_packetdef.h
---
+++ software/ovc4/ovc4_driver/src/ovc4_driver_node.cpp
@@ -0,0 +1,20 @@
+#include <ros/ros.h>
+#include <ovc4_driver/usb_driver.h>
+
+
+
+int main(int argc, char **argv)
+{
+  ROS_INFO("Hello world");
+  ros::init(argc, argv, "ovc4_driver_node");
+  ros::NodeHandle nh;
+  USBDriver usb;
+  // Send calibration read command
+  ros::Rate loop_rate(1000);
+  while (ros::ok())
+  {
+    // Poll at 1kHz
+    usb.pollData();
+    loop_rate.sleep();
+  }
+}
