680f1f200d9314dcdcc6813dff95e4ec3f1380b0
Luca Della Vedova <luca@openrobotics.org>
2020-05-14 17:06:13 +0800

    More USB cleanups
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- firmware/ovc4/lpc_fw/include/virtual_com.h
+++ firmware/ovc4/lpc_fw/include/virtual_com.h
@@ -15,35 +15,11 @@
 #define DATA_BUFF_SIZE HS_BULK_OUT_PACKET_SIZE
 
 #define USB_DEVICE_INTERRUPT_PRIORITY (3U)
-/* Currently configured line coding */
-#define LINE_CODING_SIZE (0x07)
-#define LINE_CODING_DTERATE (115200)
-#define LINE_CODING_CHARFORMAT (0x00)
-#define LINE_CODING_PARITYTYPE (0x00)
-#define LINE_CODING_DATABITS (0x08)
-
-/* Communications feature */
-#define COMM_FEATURE_DATA_SIZE (0x02)
-#define STATUS_ABSTRACT_STATE (0x0000)
-#define COUNTRY_SETTING (0x0000)
-
-/* Notification of serial state */
-#define NOTIF_PACKET_SIZE (0x08)
-#define UART_BITMAP_SIZE (0x02)
-#define NOTIF_REQUEST_TYPE (0xA1)
 
 /* Define the types for application */
 typedef struct _usb_cdc_vcom_struct
 {
     usb_device_handle deviceHandle; /* USB device handle. */
-    volatile uint8_t attach; /* A flag to indicate whether a usb device is attached. 1: attached, 0: not attached */
-    uint8_t speed;           /* Speed of USB device. USB_SPEED_FULL/USB_SPEED_LOW/USB_SPEED_HIGH.                 */
-    volatile uint8_t
-        startTransactions; /* A flag to indicate whether a CDC device is ready to transmit and receive data.    */
-    uint8_t currentConfiguration; /* Current configuration value. */
-    uint8_t currentInterfaceAlternateSetting
-        [USB_CDC_VCOM_INTERFACE_COUNT]; /* Current alternate setting value for each interface. */
-    uint8_t hasSentState; /*!< 1: The device has primed the state in interrupt pipe, 0: Not primed the state. */
 } usb_cdc_vcom_struct_t;
 
 #endif /* _USB_CDC_VCOM_H_ */
