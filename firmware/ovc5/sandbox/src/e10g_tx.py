d4b153de23ae760a0639b9609e532632ed007ae4
Morgan Quigley <morgan@osrfoundation.org>
2020-11-04 17:21:57 +0800

    tx skeleton work
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- firmware/ovc5/sandbox/src/e10g_tx.py
+++ firmware/ovc5/sandbox/src/e10g_tx.py
@@ -1,61 +1,68 @@
 from nmigen import *
 
 
-class E10G_MAC_TX(Elaboratable):
+class E10G_TX(Elaboratable):
+
     def __init__(self):
+        self.xgmii_d = Signal(64)
+        self.xgmii_c = Signal(8)
+        self.dmac = Signal(48)
+        self.smac = Signal(48)
+        self.ethertype = Signal(16)
         self.tx_d = Signal(64)
-        self.tx_c = Signal(8)
+        self.tx_en = Signal()
+
     def elaborate(self, platform):
         m = Module()
         I = 0x07
         S = 0xfb
         T = 0xfd
         E = 0xfe
-        ethertype = 0x0800
+        #ethertype = 0x0800
 
         counter = Signal(8)
         m.d.sync += counter.eq(counter + 1)
-        #m.d.comb += self.led.eq(counter[2])
+        #m.d.comb += self.led.eq(self.counter[2])
 
         with m.FSM():
             with m.State('IDLE'):
                 # todo: simplify this, move dmac/smac/type up to a UDP TX block
-                with m.If(counter == 0x42):
+                with m.If(self.tx_en):
                     m.next = 'DMAC'
                     m.d.sync += [
-                        self.tx_d.eq(0xd5555555555555fb),
-                        self.tx_c.eq(0x01)
+                        self.xgmii_d.eq(0xd5555555555555fb),
+                        self.xgmii_c.eq(0x01)
                     ]
                 with m.Else():
                     m.next = 'IDLE'
                     m.d.sync += [
-                        self.tx_d.eq(0x0707070707070707),
-                        self.tx_c.eq(0xff)
+                        self.xgmii_d.eq(0x0707070707070707),
+                        self.xgmii_c.eq(0xff)
                     ]
             with m.State('DMAC'):
                 m.next = 'SMAC'
                 m.d.sync += [
-                    self.tx_d.eq(0x3210ffffffffffff),
-                    self.tx_c.eq(0x00)
+                    self.xgmii_d.eq(0x3210ffffffffffff), # todo: concat dmac+smac[11:0]
+                    self.xgmii_c.eq(0x00)
                 ]
             with m.State('SMAC'):
                 m.next = 'PAYLOAD'
                 m.d.sync += [
-                    self.tx_d.eq(0x77770008ba987654),
-                    self.tx_c.eq(0x00),
+                    self.xgmii_d.eq(0x77770008ba987654), # todo: concat smac[47:12], ethertype, and first 16 bits payload
+                    self.xgmii_c.eq(0x00),
                     counter.eq(0)
                 ]
             with m.State('PAYLOAD'):
                 m.next = 'TERMINATE'
                 m.d.sync += [
-                    self.tx_d.eq(0x7777777777777777),
-                    self.tx_c.eq(0x00)
+                    self.xgmii_d.eq(0x7777777777777777), # todo: payload until it's empty
+                    self.xgmii_c.eq(0x00)
                 ]
             with m.State('TERMINATE'):
                 m.next = 'IDLE'
                 m.d.sync += [
-                    self.tx_d.eq(0x07070707070707fd),
-                    self.tx_c.eq(0xff),
+                    self.xgmii_d.eq(0x07070707070707fd),
+                    self.xgmii_c.eq(0xff),
                     counter.eq(0)
                 ]
 
--- firmware/ovc5/sandbox/src/sim.py
+++ firmware/ovc5/sandbox/src/sim.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from e10g_mac_tx import *
-from nmigen.back.pysim import *
+from e10g_tx import *
+from nmigen.sim import *
 
 
 '''
@@ -12,12 +12,23 @@ def wait_led(state):
 '''
 
 
-def test():
+def send_packet():
+    yield dut.tx_en.eq(1)
+    yield
+    yield dut.tx_en.eq(0)
     for i in range(100):
         yield
 
 
-dut = E10G_MAC_TX()
+def test():
+    yield dut.smac.eq(0x0123456789ab)
+    yield dut.dmac.eq(0xfedcba987654)
+    yield dut.ethertype.eq(0x0800)
+    for i in range(4):
+        yield from send_packet()
+
+
+dut = E10G_TX()
 sim = Simulator(dut)
 sim.add_clock(1e-6)
 sim.add_sync_process(test)
