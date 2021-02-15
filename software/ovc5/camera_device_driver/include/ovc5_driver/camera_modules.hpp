e1f67312df76fd331005b44b26e74bc239ac963b
Luca Della Vedova <luca@openrobotics.org>
2021-02-15 02:31:16 +0000

    Add camera module header
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

---
+++ software/ovc5/camera_device_driver/include/ovc5_driver/camera_modules.hpp
@@ -0,0 +1,17 @@
+#ifndef CAMERA_MODULES_INC
+#define CAMERA_MODULES_INC
+
+#include <ovc5_driver/camera.hpp>
+
+#define PROPRIETARY_SENSORS 1
+
+// Append all the camera headers here
+#include <ovc5_driver/cameras/picam_v2.hpp>
+//#include <ovc4_driver/cameras/picam_hq.hpp>
+#if PROPRIETARY_SENSORS
+#include <ovc5_driver/cameras/ar0521.hpp>
+#include <ovc5_driver/cameras/imx490.hpp>
+
+#endif
+
+#endif
