346e285c8a19701de21c634dcaff7b01b1a85966
Luca Della Vedova <luca@openrobotics.org>
2021-03-11 12:56:07 +0800

    Add trigger lines for latency test
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- firmware/ovc5/carrier_board/carrier_board.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ firmware/ovc5/carrier_board/carrier_board.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
-//Date        : Mon Mar  1 16:31:23 2021
+//Date        : Thu Mar 11 10:37:07 2021
 //Host        : luca-focal running 64-bit Ubuntu 20.04.2 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
