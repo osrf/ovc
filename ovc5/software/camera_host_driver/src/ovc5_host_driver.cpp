707f96751c9fd34a19ce65239ba63d42ac189188
Greg Balke <greg@openrobotics.org>
2021-04-20 14:42:35 -0700

    Default app uses libovc
    Signed-off-by: Greg Balke <greg@openrobotics.org>

--- ovc5/software/camera_host_driver/src/ovc5_host_driver.cpp
+++ ovc5/software/camera_host_driver/src/ovc5_host_driver.cpp
@@ -1,7 +1,9 @@
 #include <thread>
 #include <iostream>
 
-#include <subscriber.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <libovc/ovc.hpp>
 
 static void dumpFrame(const cv::Mat& frame)
 {
@@ -22,48 +24,17 @@ static void dumpFrame(const cv::Mat& frame)
   ran = true;
 }
 
-static cv::Mat unpackTo16(const cv::Mat& frame)
-{
-  cv::Mat ret(frame.rows, frame.cols, CV_16UC1);
-  uint8_t* frame_data = frame.data;
-  uint16_t* target_data = (uint16_t*)ret.data;
-  int idx = 0;
-  for (int r = 0; r < frame.rows; ++r)
-  {
-    for (int c = 0; c < frame.cols; c += 2)
-    {
-      // First pixel
-      target_data[r * ret.cols + c] = frame_data[idx];
-      target_data[r * ret.cols + c] |= ((uint16_t)frame_data[idx + 1] & 0xF) << 8;
-      target_data[r * ret.cols + c] <<= 4;
-      // Second pixel
-      target_data[r * ret.cols + c + 1] = frame_data[idx + 1] >> 4;
-      target_data[r * ret.cols + c + 1] |= (uint16_t)frame_data[idx + 2] << 4;
-      target_data[r * ret.cols + c + 1] <<= 4;
-      idx += 3;
-    }
-  }
-  return ret;
-}
-
 int main(int argc, char **argv)
 {
-  Subscriber sub;
-  std::thread thread(&Subscriber::receiveThread, &sub);
+  libovc::OVC ovc;
+
   while (1)
   {
-    auto frames = sub.getFrames();
+    auto frames = ovc.getFrames();
     std::cout << "Got a pair of frames" << std::endl;
-    cv::Mat frame1, frame2;
-    cv::Mat shifted1 = unpackTo16(frames[0].image);
-    cv::Mat shifted2 = unpackTo16(frames[1].image);
-    cv::cvtColor(shifted1, frame1, cv::COLOR_BayerBG2BGR);
-    cv::cvtColor(shifted2, frame2, cv::COLOR_BayerBG2BGR);
-    cv::imshow("Right", frame1);
-    //cv::imwrite("frame.png", frame1);
-    cv::imshow("Left", frame2);
+    cv::imshow("Right", frames[0].image);
+    cv::imshow("Left", frames[1].image);
     cv::waitKey(1);
   }
-  thread.join();
   return 0;
 }
