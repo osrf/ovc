d7acbf86e5d06b6ae6d04fb8efc9e0fcd3aa4b55
Luca Della Vedova <luca@openrobotics.org>
2022-01-28 17:26:03 +0800

    WIP before update
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- ovc5/firmware/carrier_board/carrier_board.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ ovc5/firmware/carrier_board/carrier_board.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
-//Date        : Mon Oct 18 14:51:09 2021
+//Date        : Fri Jan 28 17:07:07 2022
 //Host        : luca-focal running 64-bit Ubuntu 20.04.3 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
