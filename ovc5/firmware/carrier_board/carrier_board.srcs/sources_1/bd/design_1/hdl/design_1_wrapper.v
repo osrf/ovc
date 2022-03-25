0b44cee1dfb27c40a7f1a8509059fa5439273037
Luca Della Vedova <luca@openrobotics.org>
2022-03-25 15:06:12 +0800

    Streamline clocks, enforce dma coherency
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- ovc5/firmware/carrier_board/carrier_board.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ ovc5/firmware/carrier_board/carrier_board.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
-//Date        : Tue Mar 22 17:37:45 2022
+//Date        : Thu Mar 24 17:05:01 2022
 //Host        : luca-focal running 64-bit Ubuntu 20.04.4 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
