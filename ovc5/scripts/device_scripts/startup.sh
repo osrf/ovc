f0d2c8329b03f97d9565911470f3f8fd493ce4cf
Luca Della Vedova <luca@openrobotics.org>
2022-03-28 15:40:18 +0800

    Add USB hub and IRQ affinity
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- ovc5/scripts/device_scripts/startup.sh
+++ ovc5/scripts/device_scripts/startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-/root/ethernet_utils/make_usb_device
+if [ ! -f /root/C/a.out ]; then
+	echo "USB script not found, compiling"
+	g++ /root/C/hub_config.cpp -o /root/C/a.out
+fi
+/root/C/a.out
+/root/ethernet_utils/make_usb_gadget
+sleep 5;
 /root/ethernet_utils/init_usb_ethernet.sh
+
+# IRQ affinity for USB interrupts on different cores
+echo 8 > /proc/irq/71/smp_affinity
+echo 4 > /proc/irq/68/smp_affinity
